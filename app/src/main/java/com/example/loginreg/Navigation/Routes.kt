2f99335a4e8dde341b8403b78fdd6a891ccbd04f
jaymaina-8 <jaymaina19@gmail.com>
2025-04-23 17:02:58 +0300

    1 commit

---
+++ app/src/main/java/com/example/loginreg/Navigation/Routes.kt
@@ -0,0 +1,5 @@
+package com.example.loginreg.Navigation
+
+const val ROUTE_HOME="home"
+const val ROUTE_LOGIN="login"
+const val ROUTE_REGISTER="register"
---
+++ app/src/main/java/com/example/loginreg/ui/theme/Screens/jay8.kt
@@ -0,0 +1,2 @@
+package com.example.loginreg.ui.theme.Screens
+
